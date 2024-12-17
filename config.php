980f7f978c4a54015d9b7bd7d901659d2bca02a5
Gamecat999 <justjames_b@outlook.com>
2024-12-17 14:58:12 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Exchange Configuration
-define('DUCO_RECIPIENT', 'katfaucet');  // Replace with your DUCO username
-define('DUCO_PASSWORD', 'Stayout1');   // Replace with your DUCO password
-define('ELAP_USERNAME', 'gamecat999');   // Replace with your ELAP username
-define('ELAP_PASSWORD', 'Jbllc100');   // Replace with your ELAP password
+define('DUCO_RECIPIENT', 'DUCO_USERNAMEHERE');  // Replace with your DUCO username
+define('DUCO_PASSWORD', 'DUCOPASSWORDHERE');   // Replace with your DUCO password
+define('ELAP_USERNAME', 'ELAPTACOINUSERNAMEHERE');   // Replace with your ELAP username
+define('ELAP_PASSWORD', 'ELAPPASSWORDHERE');   // Replace with your ELAP password
 define('MAX_DUCO_AMOUNT', 250);
 define('MIN_DUCO_AMOUNT', 0.001);
 define('TRANSACTION_FEE', 0.02); // 2%
